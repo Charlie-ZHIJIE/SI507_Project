3acf27690ec269ffb23ec14da0e17b5e939477b9
Zhijie Xu <992571389@qq.com>
2023-04-10 14:25:01 -0400

    first draft

---
+++ Final_project.py
@@ -0,0 +1,22 @@
+#
+# Name:Zhijie Xu
+#
+import collections
+
+
+
+
+
+def main():
+    print("Welcome to Project3!")
+
+
+
+#
+# The following two-line "magic sequence" must be the last thing in
+# your file.  After you write the main() function, this line it will
+# cause the program to automatically play 20 Questions when you run
+# it.
+#
+if __name__ == '__main__':
+    main()
